7c002f80edf662b89eded69ff8f54dac52d4f4cd
Lena Ha and Nathan Barrett <lena@>
2017-10-17 16:00:14 -0700

    add (this).remove(); to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,33 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
+
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye</li>");
     $("ul#webpage").prepend("<li>Goodbye, user.  You will be the last to go.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
+
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Very well then.  You have crossed me for the last time.  You shall rue the day you have displayed such insolence, insect.</li>");
-  });
+    $("ul#webpage").prepend("<li>Very well then.  You have crossed me for the last time.  You shall rue the day you have displayed such insolence, worm.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });  
 });
